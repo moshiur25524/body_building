4bff7d7ca01de1fce181c6e124f811de90d7004d
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-11-04 03:12:37 +0600

    Service Card is created

--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './Service.css'
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, img, price, description } = service;
     return (
         <div>
-            <h1>Single Service will contain here</h1>
+            <Card border='warning' style={{ width: '18rem' }}>
+                <Card.Img variant="top" className='card-image' src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                       {description}
+                    </Card.Text>
+                    <Button variant="primary">Enroll Now</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import useServices from '../../hooks/useServices';
+import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
    const [services, setServices] = useServices()
     return (
         <div>
             <h1 className='text-center'>All the services: {services.length}</h1>
+            <div className='services'>
+                {
+                    services.map(service => <Service 
+                    key={service.id}
+                    service = {service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
