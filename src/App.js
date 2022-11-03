1f2f3db665ec1fde0186f4c2969a2f482844b6b9
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-11-03 22:29:59 +0600

    navbar making is done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Header from './Pages/Shared/Header/Header';
+import Contact from './Pages/Contact/Contact';
+import Services from './Pages/Services/Services';
+import About from './Pages/About/About';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/about' element={<About/>}/>
 
       </Routes>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={NavLink} to='/'>React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+                        <Nav.Link as={NavLink} to='/services'>Services</Nav.Link>
+                        <Nav.Link as={NavLink} to='/contact'>Contact</Nav.Link>
+                        <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
                         
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                        <Nav.Link as={NavLink} to='/login'>More deets</Nav.Link>
+                        <Button variant="outline-primary">Login</Button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
