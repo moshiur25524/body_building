f0446ba559ec7c50138b7f5abdc4c5144244e66b
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-11-03 20:30:53 +0600

    some components are created

---
+++ src/Pages/Service/Service.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <h1>Single Service will contain here</h1>
+        </div>
+    );
+};
+
+export default Service;
